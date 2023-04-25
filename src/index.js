8d5afba57006082aae5d61b8c8affe5316b46218
VMDang <dang.vm205063@sis.hust.edu.vn>
2023-04-25 21:38:24 +0700

    add context provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 
 import './index.css'
 import App from './App';
+import { ContextProvider } from "./contexts/ContextProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    <ContextProvider>
         <App />
-    </React.StrictMode>
+    </ContextProvider>
 );
