65dc97945c92d2dbc98e0eff9369279f79d2d99d
VMDang <dang.vm205063@sis.hust.edu.vn>
2023-05-06 17:09:27 +0700

    add Editor page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from "@syncfusion/ej2-react-richtexteditor";
+
+import { EditorData } from "../data/dummy";
+import { Header } from "../components";
+import {Helmet, HelmetProvider} from "react-helmet-async";
 
 const Editor = () => {
     return (
-        <div>Editor</div>
+        <HelmetProvider>
+            <Helmet>
+                <title>Editor</title>
+            </Helmet>
+
+            <div className={"m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl"}>
+                <Header title={"Editor"} category={"App"} />
+                <RichTextEditorComponent>
+                    <EditorData />
+                    <Inject services={[HtmlEditor, Toolbar, Link, QuickToolbar, Image]}/>
+                </RichTextEditorComponent>
+            </div>
+        </HelmetProvider>
     )
 }
 
